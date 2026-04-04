497fc04b89a57db9042930cd0f736fb3d3f1c5ff
jswiatly <jswiatly9@gmail.com>
2026-04-04 23:56:50 +0200

    please work

--- main.cpp
+++ main.cpp
@@ -82,6 +82,8 @@ class HelloTriangleApplication{
         VkQueue graphicsQueue;
         VkQueue presentQueue;
 
+        VkSwapChainKHR swapChain;
+
         void initWindow(){
             glfwInit();
 
@@ -174,6 +176,25 @@ class HelloTriangleApplication{
             createInfo.imageExtent = extent;
             createInfo.imageArrayLayers = 1;
             createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+            QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+            uint32_t queueFamilyIndices[] = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+            if (indices.graphicsFamily != indices.presentFamily) {
+                createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+                createInfo.queueFamilyIndices = 2;
+                createInfo.pQueueFamilyIndices  = queueFamilyIndices;
+            } else {
+                createInfo.imageSharingMode  = VK_SHARING_MODE_EXCLUSIVE;
+                createInfo.queueFamilyIndexCount = 0;
+                createInfo.pQueueFamilyIndices = nullptr;
+            }
+
+            createInfo.preTransform = swapChainSupport.capabilities.currentTransform;
+            createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+            createInfo.presentMode = presentMode;
+            createInfo.clipped = VK_TRUE;
+            createInfo.oldSwapchain = VK_NULL_HANDLE;
         }
 
         VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats){
