a196eef784f2aec98e36f4ad9675158dc652988f
jswiatly <jswiatly9@gmail.com>
2026-04-04 15:58:36 +0200

    Window Surface

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <cstring>
 #include <optional>
+#include <set>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -82,7 +83,7 @@ class HelloTriangleApplication{
             setupDebugMessenger();
             createSurface();
             pickPhysicalDevice();   
-            createLogicalInstance();
+            createLogicalDevice();
         }
 
         void createSurface(){
@@ -91,24 +92,30 @@ class HelloTriangleApplication{
             }
         }
 
-        void createLogicalInstance(){
+        void createLogicalDevice(){
             QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
-            VkDeviceQueueCreateInfo queueCreateInfo{};
-            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-            queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-            queueCreateInfo.queueCount = 1;
+            std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+            std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
 
             float queuePriority = 1.0f;
-            queueCreateInfo.pQueuePriorities = &queuePriority;
+
+            for (uint32_t queueFamily : uniqueQueueFamilies){
+                VkDeviceQueueCreateInfo queueCreateInfo{};
+                queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+                queueCreateInfo.queueFamilyIndex = queueFamily;
+                queueCreateInfo.queueCount = 1;
+                queueCreateInfo.pQueuePriorities = &queuePriority;
+                queueCreateInfos.push_back(queueCreateInfo);
+            }
 
             VkPhysicalDeviceFeatures deviceFeatures{};
 
             VkDeviceCreateInfo createInfo{};
             createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
             
-            createInfo.pQueueCreateInfos = &queueCreateInfo;
-            createInfo.queueCreateInfoCount = 1;
+            createInfo.pQueueCreateInfos = queueCreateInfos.data();
+            createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
 
             createInfo.pEnabledFeatures = &deviceFeatures;
 
@@ -126,6 +133,7 @@ class HelloTriangleApplication{
             }
 
             vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+            vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
         }
 
         void pickPhysicalDevice(){
