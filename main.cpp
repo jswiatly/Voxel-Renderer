8c96da55d512a174106c9d924a434662780b658d
jswiatly <jswiatly9@gmail.com>
2026-04-04 02:57:18 +0200

    god bless brace initialization

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ class HelloTriangleApplication{
 
         void cleanup(){
             if (enableValidationLayers) {
-                DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+               DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
             }
 
             vkDestroyInstance(instance, nullptr);
@@ -123,6 +123,7 @@ class HelloTriangleApplication{
         }
 
         void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo){
+            createInfo = {};
             createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
             createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
             createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
