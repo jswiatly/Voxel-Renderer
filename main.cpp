1064866c27dd9b9f864fbbec55f05221d7e22bb8
jswiatly <jswiatly9@gmail.com>
2026-04-04 03:59:38 +0200

    pick physical device hehe

--- main.cpp
+++ main.cpp
@@ -62,6 +62,8 @@ class HelloTriangleApplication{
 
         VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
+        VkDevice device;
+
         void initWindow(){
             glfwInit();
 
@@ -75,6 +77,38 @@ class HelloTriangleApplication{
             createInstance();
             setupDebugMessenger();
             pickPhysicalDevice();   
+            createLogicalInstance();
+        }
+
+        void createLogicalInstance(){
+            QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+            queueCreateInfo.queueCount = 1;
+
+            float queuePriority = 1.0f;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+
+            VkPhysicalDeviceFeatures deviceFeatures{};
+
+            VkDeviceCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+            
+            createInfo.pQueueCreateInfos = &queueCreateInfo;
+            createInfo.queueCreateInfoCount = 1;
+
+            createInfo.pEnabledFeatures = &deviceFeatures;
+
+            createInfo.enabledExtensionCount = 0;
+
+            if(enableValidationLayers) {
+                createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+                createInfo.ppEnabledLayerNames = validationLayers.data();
+            } else {
+                createInfo.enabledLayerCount = 0;
+            }
         }
 
         void pickPhysicalDevice(){
