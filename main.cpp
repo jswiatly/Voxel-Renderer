6185b5c94f83520fc94f99c3b5f11778c208858c
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-04-08 23:29:08 +0200

    cleanup function is a mess

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <chrono>
 #include <cstring>
 #include <optional>
 #include <set>
@@ -549,7 +552,18 @@ class HelloTriangleApplication{
         }
 
         void updateUniformBuffer(uint32_t currentImage){
+            static auto startTime = std::chrono::high_resolution_clock::now();
 
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+            UniformBufferObject ubo{};
+            ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            ubo.view =  glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            ubo.proj = glm::perspective(glm::radians(45.0f), swapChainExtent.width / (float) swapChainExtent.height, 0.1f, 10.0f);
+            ubo.proj[1][1] += -1;
+
+            memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
         }
 
         void createCommandBuffers(){
