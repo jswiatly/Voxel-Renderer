bdec9a60a89844c248731f6d351cc2eafe1c1f2f
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-04-08 23:58:04 +0200

    descriptor sets

--- main.cpp
+++ main.cpp
@@ -252,6 +252,26 @@ class HelloTriangleApplication{
             if (vkAllocateDescriptorSets(device, &allocInfo, descriptorSets.data()) != VK_SUCCESS){
                 throw std::runtime_error("failed to allocate descriptor sets!");
             }
+
+            for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++){
+                VkDescriptorBufferInfo bufferInfo{};
+                bufferInfo.buffer = uniformBuffers[i];
+                bufferInfo.offset = 0;
+                bufferInfo.range = sizeof(UniformBufferObject);
+
+                VkWriteDescriptorSet descriptorWrite{};
+                descriptorWrite.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+                descriptorWrite.dstSet = descriptorSets[i];
+                descriptorWrite.dstBinding = 0;
+                descriptorWrite.dstArrayElement = 0;
+                descriptorWrite.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                descriptorWrite.descriptorCount = 1;
+                descriptorWrite.pBufferInfo = &bufferInfo;
+                descriptorWrite.pImageInfo = nullptr; // Optional
+                descriptorWrite.pTexelBufferView = nullptr; // Optional
+
+                vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+            }
         }
 
         void createDescriptorPool(){
@@ -1146,6 +1166,8 @@ class HelloTriangleApplication{
                 vkDestroyBuffer(device, uniformBuffers[i], nullptr);
                 vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
             }
+
+            vkDestroyDescriptorPool(device, descriptorPool, nullptr);
 
             vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
 
