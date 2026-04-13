58263082e564ace13476f39db12e36b2c6e8a0b0
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-04-13 22:53:27 +0200

    maybe not

--- main.cpp
+++ main.cpp
@@ -268,6 +268,19 @@ private:
                 indices.push_back(indices.size());
             }
         }
+
+        vertex.pos = {
+            attrib.vertices[3 * index.vertex_index + 0],
+            attrib.vertices[3 * index.vertex_index + 1],
+            attrib.vertices[3 * index.vertex_index + 2]
+        };
+
+        vertex.texCoord = {
+            attrib.texcoords[2 * index.texcoord_index + 0],
+            attrib.texcoords[2 * index.texcoord_index + 1]
+        };
+
+        vertex.color = {1.0f, 1.0f, 1.0f};
     }
 
     void mainLoop() {
