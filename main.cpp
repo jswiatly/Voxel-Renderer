29f0a7bea7a9aecb34091650481caafa047c2634
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-04-13 23:22:39 +0200

    works!

--- main.cpp
+++ main.cpp
@@ -271,9 +271,9 @@ private:
         };
 
          vertex.texCoord = {
-            attrib.texcoords[2 * index.texcoord_index + 0],
-            attrib.texcoords[2 * index.texcoord_index + 1]
-        };
+    attrib.texcoords[2 * index.texcoord_index + 0],
+    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+};
 
         vertex.color = {1.0f, 1.0f, 1.0f};
 
