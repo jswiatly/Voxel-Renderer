e985e2b69d2b4da9683f693a5913103e0026935d
jswiatly <jswiatly9@gmail.com>
2026-04-23 15:21:17 +0200

    small changes

--- src/core/Device.hpp
+++ src/core/Device.hpp
@@ -22,9 +22,15 @@ const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
 };
 
+const std::vector<const char*> deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
 class VulkanDevice {
     public:
         VulkanDevice();
         ~VulkanDevice();
     private:
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        VkDevice device = VK_NULL_HANDLE;
 };
