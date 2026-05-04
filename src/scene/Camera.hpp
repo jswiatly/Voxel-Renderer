3347f621bc2def8072433cf9b31ef282c222bcd1
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-05-04 23:59:30 +0200

    Camera mouse keyboard

--- src/scene/Camera.hpp
+++ src/scene/Camera.hpp
@@ -14,8 +14,20 @@ class Camera {
         float mouseSensitivity;
 
         Camera(glm::vec3 startPos = glm::vec3(0.0f, 0.0f, 3.0f)){
+            position = startPos;
+            front = glm::vec3(0.0f, 0.0f, -1.0f);
+            up = glm::vec3(0.0f, 1.0f, 0.0f);
+            yaw = -90.0f;
+            pitch = 0.0f;
+            movementSpeed = 2.5f;
+            mouseSensitivity = 0.1f;
+        }
+
+        void processKeyboard(){
 
         }
 
+        void processMouse(){
 
+        }
 };
