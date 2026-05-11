0262bfc10fbcacc16237a75fa57f306b3eb64857
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-05-11 23:58:42 +0200

    Device wip

--- src/core/Device.hpp
+++ src/core/Device.hpp
@@ -18,11 +18,41 @@ struct SwapChainSupportDetails {
     std::vector<VkPresentModeKHR> presentModes;
 };
 
+struct VulkanDevice2
+{
+	// Physical device representation 
+	VkPhysicalDevice physicalDevice{ VK_NULL_HANDLE };;
+	// Logical device representation (application's view of the device) 
+	VkDevice logicalDevice{ VK_NULL_HANDLE };
+	// Properties of the physical device including limits that the application can check against 
+	VkPhysicalDeviceProperties properties{};
+	// Features of the physical device that an application can use to check if a feature is supported 
+	VkPhysicalDeviceFeatures features{};
+	// Features that have been enabled for use on the physical device 
+	VkPhysicalDeviceFeatures enabledFeatures{};
+	// Memory types and heaps of the physical device 
+	VkPhysicalDeviceMemoryProperties memoryProperties{};
+	// Queue family properties of the physical device 
+	std::vector<VkQueueFamilyProperties> queueFamilyProperties{};
+	// List of extensions supported by the device 
+	std::vector<std::string> supportedExtensions{};
+	// Default command pool for the graphics queue family index 
+	VkCommandPool commandPool{ VK_NULL_HANDLE };
+	// Contains queue family indices 
+};
+
+
 class VulkanDevice {
     public:
-        VulkanDevice();
-        ~VulkanDevice();
+        //VulkanDevice(VkPhysicalDevice physicalDevice);
+       // ~VulkanDevice();
     private:
+        VkInstance instance;
+        VkSurfaceKHR surface;
+        std::vector<const char*> deviceExtensions;
+        std::vector<const char*> validationLayers;
+        bool enableValidationLayers;
+
         VkPhysicalDevice physicalDevice{ VK_NULL_HANDLE };;
 	    VkDevice logicalDevice{ VK_NULL_HANDLE };
 	    VkPhysicalDeviceProperties properties{};
