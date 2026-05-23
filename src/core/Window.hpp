0eee1a55792e97b617979f6709b232f381cf9ccd
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-05-23 23:57:15 +0200

    Window.hpp: declare class skeleton

--- src/core/Window.hpp
+++ src/core/Window.hpp
@@ -1,15 +1,25 @@
 # pragma once
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <string>
 
-const uint32_t WIDTH = 1920;
-const uint32_t HEIGHT = 1080;
+int WIDTH = 800;
+int HEIGHT = 800;
 
-class VestaWindow {
+class Window {
 public:
-    VestaWindow(int w, int h, std::string name);
-    ~VestaWindow();
+    Window(int w, int h, std::string title);
+    ~Window();
+
+    Window(const Window&)            = delete;
+    Window& operator=(const Window&) = delete;
+
+    bool shouldClose() const;
+    void pollEvents() const;
+    GLFWwindow* handle() const { return window_; }
 private:
+    GLFWwindow* window_;
+    int width_;
+    int height_;
+    std::string title_;
 };
