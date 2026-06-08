017b2d40408d06b37650fdfe36ed48c339be23f0
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-06-08 23:01:02 +0200

    cleanup

--- src/scene/Sky.cpp
+++ src/scene/Sky.cpp
@@ -4,7 +4,7 @@ glm::vec4 getSkyColor(float timeOfDay){
     for (size_t i{}; i + 1 < SKY_KEYFRAMES.size(); ++i){
         const auto& a = SKY_KEYFRAMES[i];
         const auto& b = SKY_KEYFRAMES[i + 1];
-        if (timeOfDay >= a.timeOfDay && timeOfDay <= b.timeOfDay);{
+        if (timeOfDay >= a.timeOfDay && timeOfDay <= b.timeOfDay){
             float t = (timeOfDay - a.timeOfDay) / (b.timeOfDay - a.timeOfDay);
             return glm::mix(a.color, b.color, t);
         }
