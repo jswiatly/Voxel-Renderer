4616e93488d32cd57b68ec41e781840dc80bc7b5
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-06-13 23:59:32 +0200

    refactor: extract ValidationLogger from Application

--- src/core/Debug.hpp
+++ src/core/Debug.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+
 #include <mutex>
 #include <string>
 #include <vector>
@@ -8,13 +9,29 @@
 class ValidationLogger {
     public:
 
+        VkDebugUtilsMessengerCreateInfoEXT makeCreateInfo();
+        
+        void setup(VkInstance instance);
+
+        void cleanup(VkInstance instance);
+ 
+        void drawImGuiWindow();
+
     private:
+
         struct Entry {
-        VkDebugUtilsMessageSeverityFlagBitsEXT severity;
-        std::string message;
+            VkDebugUtilsMessageSeverityFlagBitsEXT severity;
+            std::string message;
         };
+
+        static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                        VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                        void* pUserData);
+
+        void append(VkDebugUtilsMessageSeverityFlagBitsEXT severity, const char* message);
 
         VkDebugUtilsMessengerEXT m_messenger = VK_NULL_HANDLE;
         std::vector<Entry> m_logs;
-        std::mutex m_mutex
-}
+        std::mutex m_mutex;
+};
