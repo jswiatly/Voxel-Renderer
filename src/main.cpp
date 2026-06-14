3fbd98c1525606c918751534a71ed9321c1ddf84
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-06-14 23:49:18 +0200

    quick fix

--- src/main.cpp
+++ src/main.cpp
@@ -22,8 +22,10 @@
 #include <unordered_map>
 
 #include "Time.hpp"
+#include "core/VulkanContext.hpp"
 #include "core/Window.hpp"
 #include "core/Debug.hpp"
+#include "core/VulkanContext.hpp"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_vulkan.h"
@@ -49,20 +51,6 @@ const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"
 
 const std::vector<const char*> deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
-struct QueueFamilyIndices {
-    std::optional<uint32_t> graphicsFamily;
-    std::optional<uint32_t> presentFamily;
-
-    bool isComplete() {
-        return graphicsFamily.has_value() && presentFamily.has_value();
-    }
-};
-
-struct SwapChainSupportDetails {
-    VkSurfaceCapabilitiesKHR capabilities;
-    std::vector<VkSurfaceFormatKHR> formats;
-    std::vector<VkPresentModeKHR> presentModes;
-};
 
 void SetupVestaStyle() {
     ImGuiStyle& style = ImGui::GetStyle();
