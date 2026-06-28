73fae0fb3f9caecec50d42fb2fd5cae70e0d558b
Julian Światły <62251523+jswiatly@users.noreply.github.com>
2026-06-29 00:03:31 +0200

    feature: backface culling

--- src/core/Pipeline.cpp
+++ src/core/Pipeline.cpp
@@ -143,7 +143,7 @@ void Pipeline::createGraphicsPipeline() {
                                                       .depthClampEnable = VK_FALSE,
                                                       .rasterizerDiscardEnable = VK_FALSE,
                                                       .polygonMode = VK_POLYGON_MODE_FILL,
-                                                      .cullMode = VK_CULL_MODE_NONE,
+                                                      .cullMode = VK_CULL_MODE_BACK_BIT,
                                                       .frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
                                                       .depthBiasEnable = VK_FALSE,
                                                       .lineWidth = 1.0f};
